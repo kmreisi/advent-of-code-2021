4fcf1a8e7cd1bd06de59b2a770f38806c1516a16
Karim Mreisi <karim.mreisi@intel.com>
2021-12-04 18:48:45 +0100

    merge days, every day one project

--- 02/main.cpp
+++ 02/main.cpp
@@ -16,53 +16,106 @@ int main(int argc, char* argv[])
   }
 
   std::string path = argv[1];
-
-  auto stream = std::ifstream(path);
-  if (!stream)
   {
-    std::cout << "Failed to read file, can not open stream.";
-    return -2;
+    // 02
+    auto stream = std::ifstream(path);
+    if (!stream)
+    {
+      std::cout << "Failed to read file, can not open stream.";
+      return -2;
+    }
+
+    size_t x = 0;
+    size_t y = 0;
+
+    std::string line;
+    while (std::getline(stream, line))
+    {
+      auto segments = split(line, " ", true, true);
+
+      if (segments.size() != 2)
+      {
+        std::cout << "Failed to parse file, a line must contain two segemnts" << line << std::endl;
+        return -3;
+      }
+
+      // read value
+      size_t value;
+      std::stringstream(segments[1]) >> value;
+
+      if (segments[0] == "forward")
+      {
+        x += value;
+      }
+      else if (segments[0] == "down")
+      {
+        y += value;
+      }
+      else if (segments[0] == "up")
+      {
+        y -= value;
+      }
+      else
+      {
+        std::cout << "Failed to parse file, missing direction" << line << std::endl;
+        return -3;
+      }
+    }
+
+    std::cout << "02: reached " << x << ":" << y << "=" << x * y << std::endl;
   }
 
-  size_t x = 0;
-  size_t y = 0;
-
-  std::string line;
-  // Get first line which should be containing the header
-  while (std::getline(stream, line))
   {
-    auto segments = split(line, " ", true, true);
+    // 02b
 
-    if (segments.size() != 2)
+    auto stream = std::ifstream(path);
+    if (!stream)
     {
-      std::cout << "Failed to parse file, a line must contain two segemnts" << line << std::endl;
-      return -3;
+      std::cout << "Failed to read file, can not open stream.";
+      return -2;
     }
 
-    // read value
-    size_t value;
-    std::stringstream(segments[1]) >> value;
+    size_t aim = 0;
+    size_t x   = 0;
+    size_t y   = 0;
 
-    if (segments[0] == "forward")
+    std::string line;
+    while (std::getline(stream, line))
     {
-      x += value;
+      auto segments = split(line, " ", true, true);
+
+      if (segments.size() != 2)
+      {
+        std::cout << "Failed to parse file, a line must contain two segemnts" << line << std::endl;
+        return -3;
+      }
+
+      // read value
+      size_t value;
+      std::stringstream(segments[1]) >> value;
+
+      if (segments[0] == "forward")
+      {
+        x += value;
+        y += aim * value;
+      }
+      else if (segments[0] == "down")
+      {
+        aim += value;
+      }
+      else if (segments[0] == "up")
+      {
+        aim -= value;
+      }
+      else
+      {
+        std::cout << "Failed to parse file, missing direction" << line << std::endl;
+        return -3;
+      }
     }
-    else if (segments[0] == "down")
-    {
-      y += value;
-    }
-    else if (segments[0] == "up")
-    {
-      y -= value;
-    }
-    else
-    {
-      std::cout << "Failed to parse file, missing direction" << line << std::endl;
-      return -3;
-    }
+
+    std::cout << "02b: reached " << x << ":" << y << "=" << x * y << std::endl;
   }
-
-  std::cout << "reached " << x << ":" << y << "=" << x*y << std::endl;
 
   return 0;
 }
--- 03/main.cpp
+++ 03/main.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <bitset>
 #include <fstream>
 #include <iostream>
@@ -7,56 +8,108 @@
 
 #include "../utils.h"
 
-int main(int argc, char* argv[])
+
+#if 0
+static std::string INPUT_PATH = "C:\\data\\projects\\advent\\03b\\example.txt";
+static const int INPUT_WIDTH   = 5;
+#else
+static std::string INPUT_PATH = "C:\\data\\projects\\advent\\03b\\input.txt";
+static const int INPUT_WIDTH  = 12;
+#endif
+
+static std::bitset<INPUT_WIDTH> calc(std::vector<std::bitset<INPUT_WIDTH>> values, bool high)
 {
-  // read file
-  if (argc != 2)
+  for (int pos = INPUT_WIDTH - 1; pos >= 0; --pos)
   {
-    std::cout << "Failed to read file, provide the path as argument." << std::endl;
-    return -1;
+    if (values.size() == 1)
+    {
+      break;
+    }
+
+    size_t set =
+        std::count_if(
+            values.begin(), values.end(), [pos](const auto& value) { return value[pos] == 1; }) *
+        10;
+
+    size_t half = (values.size() * 10) / 2;
+    bool common = set >= half;
+
+    if (!high)
+    {
+      common = !common;
+    }
+
+    // remove all values which are not in common
+    values.erase(std::remove_if(values.begin(),
+                                values.end(),
+                                [pos, common](const auto& value) { return value[pos] != common; }),
+                 values.end());
   }
 
-  std::string path = argv[1];
-  auto stream = std::ifstream(path);
+  return values[0];
+}
+
+int main(int argc, char* argv[])
+{
+
+  auto stream = std::ifstream(INPUT_PATH);
   if (!stream)
   {
     std::cout << "Failed to read file, can not open stream.";
     return -2;
   }
 
-  static const int size = 12;
-  size_t total          = 0;
-  size_t cnt[size];
-  std::fill(cnt, cnt + size, 0);
+  std::vector<std::bitset<INPUT_WIDTH>> values;
 
   std::string line;
+  // read in values
   while (std::getline(stream, line))
   {
     line = trim(line);
-    ++total;
+    std::bitset<INPUT_WIDTH> value(line);
+    values.emplace_back(value);
+  }
 
-    std::bitset<size> value(line);
-    for (int pos = 0; pos < size; ++pos)
+  {
+    // 03
+    size_t half = values.size() / 2;
+    std::bitset<INPUT_WIDTH> gama;
+    std::bitset<INPUT_WIDTH> epsilon;
+
+    size_t cnt[INPUT_WIDTH];
+    std::fill(cnt, cnt + INPUT_WIDTH, 0);
+
+    for (const auto& value : values)
     {
-      if (value[pos])
+      for (int pos = 0; pos < INPUT_WIDTH; ++pos)
       {
-        ++cnt[pos];
+        if (value[pos])
+        {
+          ++cnt[pos];
+        }
       }
     }
+
+    for (int pos = 0; pos < INPUT_WIDTH; ++pos)
+    {
+      gama[pos] = cnt[pos] > half;
+    }
+    epsilon = ~gama;
+
+    std::cout << "03: gama " << gama << std::endl;
+    std::cout << "03: epsilon " << epsilon << std::endl;
+    std::cout << "03: power " << gama.to_ullong() * epsilon.to_ullong() << std::endl;
   }
 
-  size_t half = total / 2;
-  std::bitset<size> gama;
-  std::bitset<size> epsilon;
-  for (int pos = 0; pos < size; ++pos)
   {
-    gama[pos] = cnt[pos] > half;
-  }
+    // 03b
+    auto oxygen = calc(values, true);
+    auto co2    = calc(values, false);
 
-  epsilon = ~gama;
-  std::cout << "GAMA " << gama << " " << gama.to_ullong() << std::endl;
-  std::cout << "EPSILON " << epsilon << " " << epsilon.to_ullong() << std::endl;
-  std::cout << "POWER " << gama.to_ullong() * epsilon.to_ullong() << std::endl;
+    std::cout << "03b: oxygen " << oxygen << std::endl;
+    std::cout << "03b: co2 " << co2 << std::endl;
+    std::cout << "03b: life support rating " << oxygen.to_ullong() * co2.to_ullong() << std::endl;
+  }
 
   return 0;
 }
